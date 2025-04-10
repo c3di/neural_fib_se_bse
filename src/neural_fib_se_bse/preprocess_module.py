cb27915ae80129800d0cb674997ca7afffe76eba
Tim Dahmen <Tim.Dahmen@hs-aalen.de>
2025-04-10 08:48:58 +0200

    cleaned CMakeLists.txt and directory

--- src/neural_fib_se_bse/preprocess_module.py
+++ src/neural_fib_se_bse/preprocess_module.py
@@ -3,13 +3,12 @@
 import os
 import typing
 
-print( "importing preprocess_module" )
+cuda_path = os.environ['CUDA_PATH']
+os.add_dll_directory(cuda_path + "/bin" )
 
 if not typing.TYPE_CHECKING and os.getenv("PYBIND11_PROJECT_PYTHON_DEBUG"):
-    print( "importing debug package" )
     from ._preprocess_module_d import *  # noqa: F403
     from ._preprocess_module_d import __version__  # noqa: F401, RUF100
 else:
-    print( "importing release package" )
     from ._preprocess_module import *  # noqa: F403
     from ._preprocess_module import __version__  # noqa: F401, RUF100
